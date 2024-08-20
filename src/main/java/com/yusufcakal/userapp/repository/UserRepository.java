1c0d7496a49f334118087cd47d46caf58ac0d6b4
Mathiyazahan <73736289+Mathiyazahan@users.noreply.github.com>
2024-08-20 20:01:34 +0530

    Add files via upload

---
+++ src/main/java/com/yusufcakal/userapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yusufcakal.userapp.repository;
+
+import com.yusufcakal.userapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
